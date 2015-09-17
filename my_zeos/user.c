6c755869415de77fd334d119eeca874d6b22053d
felixramos <felixramosmusic@yahoo.es>
2015-09-17 16:09:32 +0200

    added inner, outer, add functions

--- my_zeos/user.c
+++ my_zeos/user.c
@@ -4,6 +4,35 @@ char buff[24];
 
 int pid;
 
+long inner(long n)
+{
+	int i;
+	long suma;
+	suma = 0;
+	for(i=0; i<n; i++) suma += i;
+	return suma;	
+}
+
+long outer(long n)
+{
+	int i;
+	long acum;
+	acum = 0;
+	for(i=0; i<n; i++) acum = acum + inner(i);
+	return acum;
+}
+
+int add(int par1, int par2)
+{
+	int sum;
+	__asm(
+				"add %1, %2;"
+				: "=a" (sum)
+				: "a" (par1), "b" (par2)
+	);
+	return sum;
+}
+
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
